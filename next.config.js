269fd17695f9d7b11876d2b43090ed0cb94fcf81
Jason Chang <jasonchang101@gmail.com>
2024-12-01 19:41:29 +1100

    font medium

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['res.cloudinary.com', 'image.mux.com'],
-  },
-  transpilePackages: ['@mux/mux-player-react']
-}
-
-module.exports = nextConfig 
--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['res.cloudinary.com'],
+    domains: ['res.cloudinary.com', 'image.mux.com'],
   },
+  transpilePackages: ['@mux/mux-player-react']
 };
 
 export default nextConfig;
