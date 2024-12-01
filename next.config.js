a915f2f78729fd7a0d5842a05da6e6bd167e1d8a
Jason Chang <jasonchang101@gmail.com>
2024-12-01 18:32:26 +1100

    rm mp4

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com', 'image.mux.com'],
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.json$/,
-      type: 'json',
-    })
-    return config
-  },
   transpilePackages: ['@mux/mux-player-react']
 }
 
